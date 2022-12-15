e62efc9e160d8e7112b472243007c21cf3bfba3e
luki1oo1 <choragwicki.lukasz@gmail.com>
2022-12-15 08:58:05 +0100

    add Employee list

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,17 +1,17 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
 
 @Component({
   selector: 'employee-list',
   templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  data$: Observable<EmployeeModel[] | null> = of(null);
+  data$: Observable<EmployeeModel[] | null> =this._httpClient.get<EmployeeModel[]>('https://eqsfaxnghe.cfolks.pl/assets/data/employees.json');
+
+  constructor(private _httpClient: HttpClient) {}
 }
