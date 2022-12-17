dfa67e849f53bffeffa572568d1ebab0fa926693
luki1oo1 <choragwicki.lukasz@gmail.com>
2022-12-17 16:51:53 +0100

    Display alert with from data

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -9,11 +9,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup(
-    { name: new FormControl(null, [Validators.required]),
+    { email: new FormControl(null, [Validators.required]),
               age: new FormControl(null, [Validators.min(18), Validators.required]),
               salary: new FormControl(null, [Validators.required])
     });
 
   onEmployeeFormSubmitted(employeeForm: FormGroup): void {
+    alert(`User was successfully aded to the database. Email: ${employeeForm.value.email}, Age: ${employeeForm.value.age}, Salary: ${employeeForm.value.salary}`)
   }
 }
