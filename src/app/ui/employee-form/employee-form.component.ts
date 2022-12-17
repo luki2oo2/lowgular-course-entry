ec163bdd6ecc5f0338998c3ff3b47363b33b9dfe
luki1oo1 <choragwicki.lukasz@gmail.com>
2022-12-17 22:04:26 +0100

    create employee

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -9,12 +11,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup(
-    { email: new FormControl(null, [Validators.required]),
-              age: new FormControl(null, [Validators.min(18), Validators.required]),
-              salary: new FormControl(null, [Validators.required])
+    {
+      email: new FormControl(null, [Validators.required]),
+      age: new FormControl(null, [Validators.min(18), Validators.required]),
+      salary: new FormControl(null, [Validators.required])
     });
+
+  constructor(private _employeeService: EmployeeService) {
+  }
 
   onEmployeeFormSubmitted(employeeForm: FormGroup): void {
     alert(`User was successfully aded to the database. Email: ${employeeForm.value.email}, Age: ${employeeForm.value.age}, Salary: ${employeeForm.value.salary}`)
+  }
+
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create({
+      email: form.email, age: form.age, salary: form.salary
+    }).subscribe({
+      next(){ alert(`User was successfully aded to the database. Email: ${form.email}, Age: ${form.age}, Salary: ${form.salary}`)}});
   }
 }
