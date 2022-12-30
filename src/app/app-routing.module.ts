3ed29e5741517a14dfbcb7b89d659a3bd1fdce95
luki1oo1 <choragwicki.lukasz@gmail.com>
2022-12-30 15:15:26 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './ui/employee-form/employee-form.component';
+import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
+import { EmployeeFormComponentModule } from './ui/employee-form/employee-form.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'employee-list', component: EmployeeListComponent }, { path: 'employee-form', component: EmployeeFormComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, EmployeeFormComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
